5d386ef02f56087a20fcf8f3c92fe9a222b52603
Aliya <aliya.ismagilova99@gmail.com>
2024-02-20 18:49:41 -0800

    may be terrible changes, just saving to go to main real quick and do a hotfix

--- src/components/NewStylistTypeform.js
+++ src/components/NewStylistTypeform.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Modal } from "antd/lib";
-import { Widget } from "@typeform/embed-react";
+
+import Script from 'next/script'
+import Tally from 'tally-js'
 
 export default function NewStylistTypeform({
   newStylistModalOpen,
@@ -8,19 +10,22 @@ export default function NewStylistTypeform({
   handleFinishTypeformAndRedirect,
   hiddenUserEmail,
 }) {
+
   return (
     <Modal
       open={newStylistModalOpen}
       onCancel={handleCancelNewStylist}
       footer={null}
     >
-      <Widget
-        id="Y5DM4C0s"
-        className="typeform"
-        style={{ width: "100%", height: "500px" }}
-        onSubmit={handleFinishTypeformAndRedirect}
-        hidden={{ useremail: hiddenUserEmail }}
-      />
+      <iframe data-tally-src="https://tally.so/embed/3Nokvj?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1" width="100%" height="284" frameBorder="0" marginHeight="0" marginWidth="0" title="Contact form"></iframe>
+      <Script
+                id="tally-js"
+                src="https://tally.so/widgets/embed.js"
+                onLoad={() => {
+                    Tally.loadEmbeds();
+                }}
+                onSubmit={()}
+            />
     </Modal>
   );
 }
