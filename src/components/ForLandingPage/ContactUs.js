037e0ffe896ca5524fe5d4039fa32720c4cb9f5e
Aliya <aliya.ismagilova99@gmail.com>
2024-05-27 23:21:21 -0700

    addcontactpage and cosmetic changes

---
+++ src/components/ForLandingPage/ContactUs.js
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "../../styles/LandingPageMain.module.css";
+
+export default function ContactUs() {
+  return <div>hey</div>;
+}
--- src/components/ForLandingPage/ScreensSectionTwo.js
+++ src/components/ForLandingPage/ScreensSectionTwo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "../../styles/LandingPageMain.module.css";
+import { StarFilled } from "@ant-design/icons";
 
 export default function ScreensSectionTwo() {
   return (
@@ -9,7 +10,24 @@ export default function ScreensSectionTwo() {
           Friendly, contextual, informative messaging. Unique and personable.{" "}
           <b>Every time.</b>
         </span>
-        <span className={styles.textBullets}></span>
+
+        <div className={styles.textBullets}>
+          <div className={styles.textBulletsTitles}>
+            <StarFilled /> <b>No Spam. Ever.</b>
+          </div>
+          <br />
+          Clients receive only a few texts a month. Auto-removed from waitlist
+          if they book/get multiple texts. Customisable alert frequency.
+          <br />
+          <br />
+          <br />
+          <div className={styles.textBulletsTitles}>
+            <StarFilled /> <b>Interactive Texts</b>
+          </div>
+          <br />
+          AI assistant alerts you when a client is interested in booking an
+          appointment. Responds to client if the slot has been filled.
+        </div>
       </div>
       <div className={styles.screensSectionTwoImageWrapper}>
         <img
